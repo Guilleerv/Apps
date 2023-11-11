7f815f66cdf93cc47f15747c04c86edfaffae53e
Guilleerv <gramosvillenas@gmail.com>
2023-11-11 01:24:48 -0300

    services

---
+++ visor_pdf/lib/services/pdf_service.dart
@@ -0,0 +1,22 @@
+// lib/services/pdf_service.dart
+import 'package:file_picker/file_picker.dart';
+
+class PdfService {
+  Future<String?> pickAndLoadPdf() async {
+    try {
+      final filePath = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf'],
+      );
+
+      if (filePath != null && filePath.files.isNotEmpty) {
+        return filePath.files.first.path;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("Error al seleccionar el PDF: $e");
+      return null;
+    }
+  }
+}
